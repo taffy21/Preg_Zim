437e9fc522509e477d4f615316a704d6d73746c7
taffy21 <44449823+taffy21@users.noreply.github.com>
2022-07-31 22:17:23 +0200

    Application Update

---
+++ application.py
@@ -0,0 +1,83 @@
+from turtle import back
+import pygame
+from pygame.locals import *
+import sys
+import pygwidgets
+
+# CONSTANTS
+BLACK = (40, 40, 40)
+WHITE = (255, 255, 255)
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 600
+FRAMES_PS = 30
+
+# INITIALISATIONS
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Pregnancy Calculator")
+clock = pygame.time.Clock()
+
+# ASSETS
+
+# VARIABLES
+text = """Welcome to the Pregnancy App"""
+text2 = """This App that gives you all the details 
+on how to have a successful pregnancy 
+in Zimbabwe"""
+
+intro_screen = pygwidgets.DisplayText(window, (50, 100), text, fontSize = 25, width = 300, textColor=WHITE)
+intro_screen2 = pygwidgets.DisplayText(window, (75, 300), text2, textColor=WHITE, justified='center')
+
+page_two_button = pygwidgets.TextButton(window, (150, 500), "Enter App")
+page_two = False
+label = "What Is Your Name: "
+label_screen = pygwidgets.DisplayText(window, (0, 100), label, textColor=WHITE, fontSize=30)
+input_box = pygwidgets.InputText(window, (200, 100), fontSize=30, width=150)
+
+
+page_three = False
+page_three_button = pygwidgets.TextButton(window, (150, 300), "Next")
+back_button = pygwidgets.TextButton(window, (150, 400), "Back" )
+
+# LOOPS
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if page_two_button.handleEvent(event):
+            page_two = True
+        if input_box.handleEvent(event):
+            input_text = input_box.getValue()
+            #name_text = pygwidgets.DisplayText(window, (0, 100), input_text, textColor=WHITE)
+        if page_three_button.handleEvent(event):
+            page_two = False
+            page_three = True
+        if back_button.handleEvent(event):
+            page_two = False
+            page_three = False
+        
+    if page_two:
+        window.fill(BLACK)
+        label_screen.draw()
+        input_box.draw()
+        page_three_button.draw()
+        back_button.draw()
+        pygame.display.update()
+        clock.tick(FRAMES_PS)
+
+    elif page_three:
+        window.fill(BLACK)
+        #name_text.draw()
+        pygame.display.update()
+        clock.tick(FRAMES_PS)
+    else:
+
+        window.fill(BLACK)
+        intro_screen.draw()
+        intro_screen2.draw()
+        page_two_button.draw()
+        pygame.display.update()
+        clock.tick(FRAMES_PS)
+
+
